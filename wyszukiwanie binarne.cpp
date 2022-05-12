0490042b39229d3aa1e7a40be3c00bb4c6ac48f1
Hubert105 <99913416+Hubert105@users.noreply.github.com>
2022-05-12 10:30:56 +0200

    Add files via upload

---
+++ wyszukiwanie binarne.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int szukana;
+	int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	int pozycja;
+A:
+	int l, p, sr;
+	l = 0;
+	p = 15;
+	sr = (l + p) / 2;
+
+
+
+	cout << "Jaka liczbe chcesz znalezc: ";
+	cin >> szukana;
+
+
+
+	while (l <= p)
+	{
+		if (tab[sr] == szukana)
+		{
+			cout << "liczba " << szukana << " jest w " << sr;
+			return 0;
+		}
+		if (tab[sr] > szukana)
+		{
+			p = sr - 1;
+		}
+		else
+		{
+			l = sr + 1;
+		}
+		sr = (l + p) / 2;
+	}
+	cout << "nie ma takiej liczby";
+}
