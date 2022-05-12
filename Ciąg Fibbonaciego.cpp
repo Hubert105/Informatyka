0b1d0abf98874f99978028db68ce075988ad40dc
Hubert105 <99913416+Hubert105@users.noreply.github.com>
2022-05-12 10:39:37 +0200

    Add files via upload

---
+++ Ciąg Fibbonaciego.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int n = 0;
+	cout << "liczba";
+	cin >> n;
+
+
+
+	int f;
+	int f0 = 0;
+	int f1 = 1;
+
+
+
+	for (int i = 0; i <= n; i++)
+	{
+		if (i > 1)
+		{
+			f = f0 + f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else
+		{
+			f = i;
+		}
+		cout << f << "\n";
+	}
+
+
+
+}
